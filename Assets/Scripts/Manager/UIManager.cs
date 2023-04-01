c0421c100d8c222c9bb61b3a9c9d4e44835e16d6
Rang_ddang <62083659+rangddang@users.noreply.github.com>
2023-04-01 13:21:19 +0900

    updateScore

--- Assets/Scripts/Manager/UIManager.cs
+++ Assets/Scripts/Manager/UIManager.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class UIManager : MonoBehaviour
 {
     [SerializeField] private GameObject GameOverPanel;
+    [SerializeField] private TextMeshProUGUI ScoreText;
 
     private void Start()
     {
@@ -18,5 +20,6 @@ public class UIManager : MonoBehaviour
         {
             GameOverPanel.SetActive(true);
         }
+        ScoreText.text = DataManager.Instance.Score.ToString();
     }
 }
